9a641187cd3fbbc9f8c111675741002610d7e8f3
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-09 20:01:50 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,10 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
+  -Version "2.0.0" `
   -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -OutputFilePrefix "Riot_Act_1.0" `
+  -ModName "Half-Life 2: Riot Act 1.0" `
+  -ModFolder "half-life 2 riot act" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/hl2-riot-act/Riot-Act-1.0-Original.zip" `
+  -ModBaseFilesUrlHash "cbc508a09dc0cdf17773e29e52e3305245e673af74d40f3e109766efc6eb7dea" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
